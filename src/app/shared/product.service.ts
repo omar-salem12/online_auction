83059198596e88ca7c93d6d1893a05bbae12968a
Omar-Salem12 <omarsalem20200@gmail.com>
2021-07-01 14:50:28 +0200

    added product services

--- src/app/shared/product.service.ts
+++ src/app/shared/product.service.ts
@@ -1,9 +1,64 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+
 
 @Injectable({
   providedIn: 'root'
 })
+
+export class Product {
+  
+    
+
+    constructor(
+    @Inject(Number)public id: number,
+    @Inject(Number)  public price: number,
+    @Inject(Number)   public rating: number,
+    @Inject(String) public title: string,
+    @Inject(String) public description: string,
+    @Inject(String)  public categories: string[],
+    ) {}
+    
+}
+ 
 export class ProductService {
 
-  constructor() { }
-}
+ getProducts(): Product[] {
+   return products.map(p => new Product(p.id, p.price,p.rating,p.title,p.description,p.categories))
+ }
+
+  
+
+  
+  getProductById(productId: number): Product | undefined {
+
+  return products.find(p => p.id === productId);
+
+  }
+
+  }
+
+
+
+
+
+
+
+const products  = [
+  {
+     'id': 0,
+     'title': 'First Product',
+      'price': 24.99,
+      'rating' : 4.3,
+      'description': 'this is a short descripton Lorem ipsum dolor sit amet, consectur adipiacing elit.',
+      'categories' : ['electronics', 'hardware']
+  },
+
+  {
+    'id': 1,
+    'title': 'First Product',
+     'price': 45.99,
+     'rating' : 3.3,
+     'description': 'this is a short descripton Lorem ipsum dolor sit amet, consectur adipiacing elit.',
+     'categories' : ['books']
+ }
+]
